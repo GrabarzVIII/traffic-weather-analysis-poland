8d85f905791ebe2289ae41e064e4c1dad52e5fe3
GrabarzVIII <kacperlukaszgrabarczyk@gmail.com>
2025-06-10 15:26:00 +0200

    GDDiK xml download func creadted

--- Scripts/GDDiK_Download.py
+++ Scripts/GDDiK_Download.py
@@ -3,84 +3,91 @@ import os
 import xml.etree.ElementTree as ET
 import datetime
 
-def saving_xml(xml_file,data_path):
-    gen = ""
-    with open(data_temp_path, 'wb') as f:
-        f.write(xml_file.content)
-    
-    tree = ET.parse(data_temp_path)
-    root = tree.getroot()
-    gen = root.attrib.get('gen')
-    gen_file = gen.replace(":","")
+from utils.const import DATA_GDDIK_PATH, DATA_GDDIK_TEMP_PATH, DATA_GDDIK_LOG_PATH, URL_GDDIK
 
-    data_path = f'{data_path}\\GDDiK_data_{gen_file}'
+def gddik_xml_downloading():
 
-    with open(data_path, 'wb') as f:
-        f.write(xml_file.content)
-
-    os.remove(data_temp_path)
-
-    with open(log_path, 'a') as f:
-        f.write(f'{time}-{data_path}-saved\n')
-
-data_path = r"Data\GDDiK_data"
-data_temp_path = r"Data\GDDiK_data\temp_xml\temp.xml"
-log_path = r"logs\GDDiK_download.log"
-url = r"https://www.archiwum.gddkia.gov.pl/dane/zima_html/utrdane.xml"
-time = datetime.datetime.now()
-
-latest_file = None
-latest_time = 0
-
-#deleting temp file
-if os.path.exists(data_temp_path):
-    os.remove(data_temp_path)
-
-xml_file = requests.get(url)
-if xml_file.status_code == 200:
-
-    #scaning for lastest xml file form GDDiK
-    with os.scandir(data_path) as entries:
-        for entry in entries:
-            if entry.is_file():
-                mod_time = entry.stat().st_mtime
-                if mod_time > latest_time:
-                    latest_time = mod_time
-                    latest_file = entry.name
-
-
-    #if not exists previous GDDiK xml file
-    if latest_file == None:
-        saving_xml(xml_file,data_path)
-                
-    #if exists previous GDDiK xml file    
-    else:
-        data_path_l = f'{data_path}\\{latest_file}'
-
-        with open(data_temp_path,'wb') as f:
-            f.write(xml_file.content)
-
-        tree_lastest = ET.parse(data_path_l)
-        root_lastest = tree_lastest.getroot()
-        gen_lastest = root_lastest.attrib.get('gen')
-
-        tree_temp = ET.parse(data_temp_path)
-        root_temp = tree_temp.getroot()
-        gen_temp = root_temp.attrib.get('gen')
-
-
-        #if GDDiK xml was download
-        if gen_lastest == gen_temp:
-
-            with open(log_path, 'a') as f:
-                f.write(f'{time}-{data_path}-exists\n')
-
-        else:
-            saving_xml(xml_file,data_path)
-else:
-    with open(log_path, 'a') as f:
-                f.write(f'{time}-{xml_file.status_code}-faild_connection')
+    def saving_xml(xml_file: requests.models.Response,data_path: str):
+        try:
+            gen = ""
+            with open(DATA_GDDIK_TEMP_PATH, 'wb') as f:
+                f.write(xml_file.content)
             
+            tree = ET.parse(DATA_GDDIK_TEMP_PATH)
+            root = tree.getroot()
+            gen = root.attrib.get('gen')
+            gen_file = gen.replace(":","")
+
+            data_path = f'{DATA_GDDIK_PATH}\\GDDiK_data_{gen_file}'
+
+            with open(data_path, 'wb') as f:
+                f.write(xml_file.content)
+
+            os.remove(DATA_GDDIK_TEMP_PATH)
+
+            with open(DATA_GDDIK_LOG_PATH, 'a') as f:
+                f.write(f'{datetime.datetime.now()}-{data_path}-saved\n')
+
+        except Exception as e:
+            with open(DATA_GDDIK_LOG_PATH, 'a') as f:
+                f.write(f'ERROR-{datetime.datetime.now()}-{e}\n')
+
+    try:
+        latest_file = None
+        latest_time = 0
+
+        #deleting temp file
+        if os.path.exists(DATA_GDDIK_TEMP_PATH):
+            os.remove(DATA_GDDIK_TEMP_PATH)
+
+        xml_file = requests.get(URL_GDDIK)
+        if xml_file.status_code == 200:
+
+            #scaning for lastest xml file form GDDiK
+            with os.scandir(DATA_GDDIK_PATH) as entries:
+                for entry in entries:
+                    if entry.is_file():
+                        mod_time = entry.stat().st_mtime
+                        if mod_time > latest_time:
+                            latest_time = mod_time
+                            latest_file = entry.name
+
+
+            #if not exists previous GDDiK xml file
+            if latest_file == None:
+                saving_xml(xml_file,DATA_GDDIK_PATH)
+                        
+            #if exists previous GDDiK xml file    
+            else:
+                data_path_l = f'{DATA_GDDIK_PATH}\\{latest_file}' #change this line to more flexible
+
+                with open(DATA_GDDIK_TEMP_PATH,'wb') as f:
+                    f.write(xml_file.content)
+
+                tree_lastest = ET.parse(data_path_l)
+                root_lastest = tree_lastest.getroot()
+                gen_lastest = root_lastest.attrib.get('gen')
+
+                tree_temp = ET.parse(DATA_GDDIK_TEMP_PATH)
+                root_temp = tree_temp.getroot()
+                gen_temp = root_temp.attrib.get('gen')
+
+
+                #if GDDiK xml was download
+                if gen_lastest == gen_temp:
+
+                    with open(DATA_GDDIK_LOG_PATH, 'a') as f:
+                        f.write(f'{datetime.datetime.now()}-{data_path_l}-exists\n')
+
+                else:
+                    saving_xml(xml_file,DATA_GDDIK_PATH)
+        else:
+            with open(DATA_GDDIK_LOG_PATH, 'a') as f:
+                f.write(f'{datetime.datetime.now()}-{xml_file.status_code}-faild_connection')
+
+    except Exception as e:
+        with open(DATA_GDDIK_LOG_PATH, 'a') as f:
+                f.write(f'ERROR-{datetime.datetime.now()}-{e}\n')
 
 
 
---
+++ Scripts/utils/const.py
@@ -0,0 +1,4 @@
+DATA_GDDIK_PATH = r"Data\GDDiK_data"
+DATA_GDDIK_TEMP_PATH = r"Data\GDDiK_data\temp_xml\temp.xml"
+DATA_GDDIK_LOG_PATH = r"logs\GDDiK_download.log"
+URL_GDDIK = r"https://www.archiwum.gddkia.gov.pl/dane/zima_html/utrdane.xml"
