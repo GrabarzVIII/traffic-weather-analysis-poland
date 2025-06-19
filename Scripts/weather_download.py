5a7d33dcf376f8ab63fdd6c4b600da1385dd8cb8
GrabarzVIII <kacperlukaszgrabarczyk@gmail.com>
2025-06-19 23:05:17 +0200

    weather download finc final ver.

--- Scripts/weather_download.py
+++ Scripts/weather_download.py
@@ -3,12 +3,7 @@ from datetime import datetime
 from meteostat import Hourly, Point
 from utils.const import DATA_WHEATHER_LOG_PATH
 
-#lan = 17.090633
-#lat = 51.178573
-#data_string = '2025-03-24T18:36:00+0100'
-#type_u = 'U'
-
-def get_weather (lat: float, lan: float, data_str: str, type_utr:str) -> pd.core.frame.DataFrame:
+def get_weather (lat: float, lon: float, data_str: str, type_utr:str) -> pd.core.frame.DataFrame:
 
     def log(message: str):
         with open(DATA_WHEATHER_LOG_PATH, 'a') as f:
@@ -19,9 +14,13 @@ def get_weather (lat: float, lan: float, data_str: str, type_utr:str) -> pd.core
         y,m,d,h,min = int(data_str[:4]),int(data_str[5:7]),int(data_str[8:10]),int(data_str[11:13]),int(data_str[14:16])
         start = datetime(y,m,d,h,0)
         end = datetime(y,m,d,h+1,0)
-        point = Point(lat,lan)
+        point = Point(lat,lon)
         weather = Hourly(point,start=start,end=end)
         weather_df = weather.fetch()
+
+        if weather_df.empty:
+            log(f'no_weather_data_for_{type_utr}{lat}{lon}{data_str}')
+            return pd.DataFrame()
 
         weather_df = weather_df.rename(columns={
             'temp': 'temperature',
@@ -37,21 +36,21 @@ def get_weather (lat: float, lan: float, data_str: str, type_utr:str) -> pd.core
             'coco': 'weather_condition_code'
         })
         
-        weather_df['utr_index'] = f'{type_utr}{lat}{lan}{data_str}'
+        weather_df['utr_index'] = f'{type_utr}{lat}{lon}{data_str}'
         
-        if min < 20:
+        if min < 20 and start in weather_df.index:
             return weather_df.loc[[start]]
 
         elif min >= 20 and min < 40:
-            return weather_df.groupby('utr_index').mean()
+            return weather_df.groupby('utr_index').mean(numeric_only=True)
         
-        elif min >= 40:
+        elif min >= 40 and end in weather_df.index:
             return weather_df.loc[[end]]
+        
+        else:
+            return weather_df.iloc[[0]]
 
 
     except Exception as e:
         log(e)
-        return None
-
-
-#get_weather(lat=lat,lan=lan,data_str=data_string)
+        return pd.DataFrame()
