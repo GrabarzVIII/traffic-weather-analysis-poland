4b6f1c9376db6b26afac4e04cceea5d8da54f953
GrabarzVIII <kacperlukaszgrabarczyk@gmail.com>
2025-06-20 22:58:41 +0200

    added additional logs to weather_download func

--- Scripts/weather_download.py
+++ Scripts/weather_download.py
@@ -39,15 +39,19 @@ def get_weather (lat: float, lon: float, data_str: str, type_utr:str) -> pd.core
         weather_df['utr_index'] = f'{type_utr}{lat}{lon}{data_str}'
         
         if min < 20 and start in weather_df.index:
+            log(f'weather_data_for_{type_utr}{lat}{lon}{data_str}_downloaded')
             return weather_df.loc[[start]]
 
         elif min >= 20 and min < 40:
+            log(f'weather_data_for_{type_utr}{lat}{lon}{data_str}_downloaded')
             return weather_df.groupby('utr_index').mean(numeric_only=True)
         
         elif min >= 40 and end in weather_df.index:
+            log(f'weather_data_for_{type_utr}{lat}{lon}{data_str}_downloaded')
             return weather_df.loc[[end]]
         
         else:
+            log(f'weather_data_for_{type_utr}{lat}{lon}{data_str}_downloaded')
             return weather_df.iloc[[0]]
 
 
